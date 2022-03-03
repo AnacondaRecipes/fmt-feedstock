dc89049ecc2b6852496992e247f78fd1d0d3fd97
Scott Bailey <sbailey@anaconda.com>
2022-03-03 16:32:31 -0700

    8.1.1 update w/ author testing.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,30 @@
+#!/bin/bash
 
-cmake ${CMAKE_ARGS} \
-  -DCMAKE_PREFIX_PATH:PATH=${PREFIX} \
-  -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} \
-  -DCMAKE_INSTALL_LIBDIR=lib \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DBUILD_SHARED_LIBS=TRUE \
-  -DFMT_TEST=OFF \
-  -DFMT_DOC=OFF \
-  -DFMT_INSTALL=ON \
-  .
 
-make -j${CPU_COUNT} install
+# Isolate the build.
+mkdir -p Build
+cd Build || exit 1
+
+
+# Generate the build files.
+cmake .. -G"Ninja" ${CMAKE_ARGS} \
+      -DCMAKE_PREFIX_PATH=$PREFIX \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      -DBUILD_SHARED_LIBS=TRUE \
+      -DFMT_TEST=ON \
+      -DFMT_DOC=OFF \
+      -DFMT_INSTALL=ON \
+      -DCMAKE_BUILD_TYPE=Release
+
+
+# Build.
+ninja || exit 1
+
+
+# Perform tests.
+ninja test || exit 1
+
+
+# Install.
+ninja install || exit 1
