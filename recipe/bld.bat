6f88f38fcd20de5a1e7fb9711c099e0bf84dcc62
Scott Bailey <sbailey@anaconda.com>
2022-03-04 08:55:49 -0700

    Correct testing.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,29 +8,34 @@ if errorlevel 1 exit 1
 
 
 :: Generate the build files.
+echo "Generating the build files."
 cmake .. -G"Ninja" %CMAKE_ARGS% ^
       -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
       -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-      -DFMT_TEST=OFF ^
+      -DFMT_TEST=ON ^
       -DFMT_DOC=OFF ^
       -DFMT_INSTALL=ON ^
       -DCMAKE_BUILD_TYPE=Release
 
 
 :: Build.
+echo "Building..."
 ninja
 if errorlevel 1 exit 1
 
 
 :: Perforem tests.
+echo "Testing..."
 ninja test
 if errorlevel 1 exit 1
 
 
-:: Build and install.
+:: Install.
+echo "Installing..."
 ninja install
 if errorlevel 1 exit 1
 
 
 :: Error free exit.
+echo "Error free exit!"
 exit 0
